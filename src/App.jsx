5cbaed9aeb249383316490af065ea75d77de920c
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-03 20:08:47 +0530

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import { Navbar, SideNav } from "./components";
+import { History, Home, Liked, Playlists, WatchLater } from "./pages";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <SideNav />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/liked" element={<Liked />} />
+        <Route path="/playlists" element={<Playlists />} />
+        <Route path="/watch-later" element={<WatchLater />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </div>
   );
 }
