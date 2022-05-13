7c0b5682bb20d8abe4bbb08944a9e2ae48d2db30
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-13 08:45:28 +0530

    feat: add videos to database and add categories

--- src/backend/db/videos.js
+++ src/backend/db/videos.js
@@ -5,17 +5,226 @@
 
 export const videos = [
   {
-    _id: "Wo5dMEP_BbI",
-    title: "Awesome Video about Coding",
+    _id: "yRv0tk-KWuM",
+    title: "Chocolate Sea Dragon!",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
-    creator: "Soham Shah",
+      "This chocolate sea dragon weighs 90lbs and can stretch up to 7ft long.I love this new idea for showpiece where you have 3 modular elements, creating a different feeling each time you move them.",
+    videoThumbnail: "https://img.youtube.com/vi/yRv0tk-KWuM/maxresdefault.jpg",
+    channelName: "Amaury Guichon",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLT1jOJ-J328B56V0XNneTR02M1Qg7FlSSZomtCzNg=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.7M",
+    numLikes: "50k",
+    videoTime: "3:03",
+    categoryName: "Food Art",
   },
   {
-    _id: "F_Riqjdh2oM",
-    title: "Neural Networks from Scratch - P.1 Intro and Neuron Code",
-    creator: "Sentdex",
+    _id: "bDHorW-MbdI",
+    title: "The Lily Pad!",
     description:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga.",
+      "Asian inspiration for one of my favorite pastry creations #schoolofchocolate, it is composed with a crunchy matcha sweet dough, a light Japanese roll matcha sponge, a matcha cremeux, a yuzu cremeux, a yuzu jelly and a green tea matcha mousse. The flower is crafted out of 100% white chocolate!",
+    videoThumbnail: "https://img.youtube.com/vi/bDHorW-MbdI/maxresdefault.jpg",
+    channelName: "Amaury Guichon",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLT1jOJ-J328B56V0XNneTR02M1Qg7FlSSZomtCzNg=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "645k",
+    numLikes: "17k",
+    videoTime: "3:21",
+    categoryName: "Food Art",
+  },
+  {
+    _id: "BEzASBGeEIM",
+    title: "7 Healthy & Tasty Dinner Ideas for the Week (Vegetarian)",
+    description:
+      "Quick, Healthy and Tasty  Dinner Ideas after a tiring day at Office, School or College.  Healthy and tasty  Indian dinner recipes for the entire week.",
+    videoThumbnail: "https://img.youtube.com/vi/BEzASBGeEIM/maxresdefault.jpg",
+    channelName: "Fit Tuber",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLSEV5H0Dc_SHuQwWmvhnSJG4guCrWjvhTe8wlzLKA=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "4.08M",
+    numLikes: "145k",
+    videoTime: "10:17",
+    categoryName: "Healthy Recipes",
+  },
+  {
+    _id: "u66pG73UroY",
+    title: "MUGHLAI CHICKEN HANDI",
+    description: "Mughlai Chicken Handi Recipe",
+    videoThumbnail: "https://img.youtube.com/vi/u66pG73UroY/maxresdefault.jpg",
+    channelName: "Spice Eats",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLSRf1-a2C3qM5mdqC7OqChTzmQPux4RONzTXF6MVQ=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "8.5M",
+    numLikes: "148k",
+    videoTime: "5:19",
+    categoryName: "Indian Recipes",
+  },
+  {
+    _id: "u459lgZFqgk",
+    title: "The Elixir of Love!",
+    description:
+      "This love potion created for valentine's day is composed with a pink joconde sponge soaked with a strawberry and rose champagne syrup, a fresh strawberry compote, a thin layer of strawberry cremeux, a rose champagne clear jelly with pink gold flakes, a crunchy layer of naturally pink ruby chocolate and the cork is an actual strawberry and lemon lollypop.",
+    videoThumbnail: "https://img.youtube.com/vi/u459lgZFqgk/maxresdefault.jpg",
+    channelName: "Amaury Guichon",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLT1jOJ-J328B56V0XNneTR02M1Qg7FlSSZomtCzNg=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.3M",
+    numLikes: "24k",
+    videoTime: "3:11",
+    categoryName: "Food Art",
+  },
+  {
+    _id: "-77-G7E0NvQ",
+    title: "The Saturn!",
+    description:
+      " This accurate version of the Saturn is a gluten-free passion fruit cheesecake in disguise. It is composed with a marbling of cheesecake mousse and passion fruit jelly, a vanilla exotic compote in the center, a crunchy gluten-free recomposed streusel, and 2 thin shells of crunchy white chocolate.",
+    videoThumbnail: "https://img.youtube.com/vi/-77-G7E0NvQ/maxresdefault.jpg",
+    channelName: "Amaury Guichon",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLT1jOJ-J328B56V0XNneTR02M1Qg7FlSSZomtCzNg=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "481k",
+    numLikes: "7.2k",
+    videoTime: "3:24",
+    categoryName: "Food Art",
+  },
+  {
+    _id: "m0WfCE0aTEU",
+    title: "Art In FRUIT CARVING AND CUTTING TRICKS!",
+    description: "Fruit Carving Art",
+    videoThumbnail: "https://img.youtube.com/vi/m0WfCE0aTEU/maxresdefault.jpg",
+    channelName: "ItalyPaul - Art In Fruit & Vegetable Carving Lessons",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLTSLPhX2B4aHXecLjhiKElaCDCqN2dxK0qHes_v=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "3.9M",
+    numLikes: "44k",
+    videoTime: "11:57",
+    categoryName: "Food Art",
+  },
+  {
+    _id: "oEnHeO5K6-Q",
+    title: "Simple Chocolate Techniques You Need To Know",
+    description:
+      "You don’t need to be a professional to make stunning chocolate garnishes in your own kitchen – you just need the help of a few of Kirsten Tibballs chocolate hacks! ",
+    videoThumbnail: "https://img.youtube.com/vi/oEnHeO5K6-Q/maxresdefault.jpg",
+    channelName: "Kirsten Tibballs",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLQvGQTPFmGqzvVXSPrgPXWN-oIX1deSwu7DbjqPzw=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.09M",
+    numLikes: "21k",
+    videoTime: "25:34",
+    categoryName: "Food Art",
+  },
+  {
+    _id: "z161elNBhN4",
+    title: "KADAI VEG RECIPE RESTAURANT STYLE",
+    description:
+      "Kadai Veg Recipe Restaurant Style | Kadai Vegetable | Veg Kadai Recipe | How To Make Kadai Vegetable",
+    videoThumbnail: "https://img.youtube.com/vi/z161elNBhN4/maxresdefault.jpg",
+    channelName: "Spice Swad",
+    channelImg:
+      "https://yt3.ggpht.com/T3R1GYFz5sieUtSVRpA9MET6wqhrPAjtJ1hONLdr8I45dAZ6XyOFaPHZVMja6CXdD6pAcZe7yAo=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.2M",
+    numLikes: "17k",
+    videoTime: "5:42",
+    categoryName: "Indian Recipes",
+  },
+  {
+    _id: "8VBnaFhOEn8",
+    title: "Basic Knife Skills – Bruno Albouze",
+    description:
+      "Learn the culinary basic knife cuts such as fine brunoise, brunoise, small dice (macédoine), medium dice (Parmentier), large dice, paysanne, roll-cuts, diagonal and the tourner technique; a football-shaped seven curved sides product!",
+    videoThumbnail: "https://img.youtube.com/vi/8VBnaFhOEn8/maxresdefault.jpg",
+    channelName: "Bruno Albouze",
+    channelImg:
+      "https://yt3.ggpht.com/HHv92t6kYykPkYbKnHjtatK5n770H4FvJYyv-PQQludf71aL-xPCaICUUEQlrf2-hL9nsu8DURQ=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "3.2M",
+    numLikes: "49k",
+    videoTime: "4:42",
+    categoryName: "Beginner's Basics",
+  },
+  {
+    _id: "voHVFxMnMDw",
+    title: "Healthy Chicken Salad Recipe",
+    description:
+      "This chicken salad is healthy, easy to make and super delicious.  This healthy chicken salad is high in proteins, low-carb, keto-friendly and perfect salad for weight loss! If you like creamy salads, you will love this creamy chicken salad. One of the many benefits of this salad is that it goes well with sandwiches, so you can take this salad to school or work or just serve with your favorite dinner.",
+    videoThumbnail: "https://img.youtube.com/vi/voHVFxMnMDw/maxresdefault.jpg",
+    channelName: "The Cooking Foodie",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLQ68BU8BHj6In5XdGkHnMx2Px2YHRHaaqa12_o7Gw=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.7M",
+    numLikes: "45k",
+    videoTime: "3:35",
+    categoryName: "Healthy Recipes",
+  },
+  {
+    _id: "nGL7TpuaR0w",
+    title: "Honey Smoked Chicken Salad | BeerBiceps Easy Keto Salad Recipes",
+    description:
+      "This healthy chicken salad is one of the most healthy AND KETO FRIENDLY chicken recipes you'll ever come across. Keto chicken recipes have never been this legit.",
+    videoThumbnail: "https://img.youtube.com/vi/nGL7TpuaR0w/maxresdefault.jpg",
+    channelName: "BeerBiceps",
+    channelImg:
+      "https://yt3.ggpht.com/4syhBAa6d4sK_J8YxhRFHDyARee3qoR2bUtGMtRDyyVncFn41bSHUDf-FNmvcu3TD2JwowD8sA=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "40k",
+    numLikes: "813",
+    videoTime: "8:07",
+    categoryName: "Healthy Recipes",
+  },
+  {
+    _id: "mbo5wXh1lWE",
+    title: "5 Simple & Creative Recipes to Try at Home (Vegetarian)",
+    description:
+      "Simple & Creative Recipes to Try at home. These are 100% vegetarian recipes which are easy, tasty and healthy. Lockdown recipes at home (Healthy and tasty)",
+    videoThumbnail: "https://img.youtube.com/vi/mbo5wXh1lWE/maxresdefault.jpg",
+    channelName: "Fit Tuber",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLSEV5H0Dc_SHuQwWmvhnSJG4guCrWjvhTe8wlzLKA=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.14M",
+    numLikes: "57k",
+    videoTime: "8:44",
+    categoryName: "Healthy Recipes",
+  },
+  {
+    _id: "yRv0tk-KWuM",
+    title: "Chocolate Sea Dragon!",
+    description:
+      "This chocolate sea dragon weighs 90lbs and can stretch up to 7ft long.I love this new idea for showpiece where you have 3 modular elements, creating a different feeling each time you move them.",
+    videoThumbnail: "https://img.youtube.com/vi/yRv0tk-KWuM/maxresdefault.jpg",
+    channelName: "Amaury Guichon",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLT1jOJ-J328B56V0XNneTR02M1Qg7FlSSZomtCzNg=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "1.7M",
+    numLikes: "50k",
+    videoTime: "3:03",
+    categoryName: "Food Art",
+  },
+  {
+    _id: "lkb5DkraJZg",
+    title:
+      "Healthy Paneer Chilli | BeerBiceps Vegetarian Indian Chinese Style Recipes",
+    description:
+      "The iconic Indian Chilli paneer is by faaar one of my all time favourite dishes. Glad that i could come up with my own fitness and bodybuilding friendly version of my favourite indo Chinese food for beerbiceps. This is the dry version of paneer chilli but making this a gravy is not an issue.",
+    videoThumbnail: "https://img.youtube.com/vi/lkb5DkraJZg/maxresdefault.jpg",
+    channelName: "BeerBiceps",
+    channelImg:
+      "https://yt3.ggpht.com/4syhBAa6d4sK_J8YxhRFHDyARee3qoR2bUtGMtRDyyVncFn41bSHUDf-FNmvcu3TD2JwowD8sA=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "147k",
+    numLikes: "3.1k",
+    videoTime: "7:01",
+    categoryName: "Healthy Recipes",
+  },
+  {
+    _id: "c01s-UVxoQk",
+    title: "25 Easy Plating Techniques - Plate like a Pro",
+    description:
+      "Follow this easy plating techniques to instantly elevate your food presentations.",
+    videoThumbnail: "https://img.youtube.com/vi/c01s-UVxoQk/maxresdefault.jpg",
+    channelName: "Home Chef Seattle",
+    channelImg:
+      "https://yt3.ggpht.com/ytc/AKedOLRgJvJgHRoM8TiT_cEMD7xG4oZPsSMXzwwq1DBy=s48-c-k-c0x00ffffff-no-rj",
+    numViews: "68k",
+    numLikes: "1.9k",
+    videoTime: "7:01",
+    categoryName: "Beginner's Basics",
   },
 ];
