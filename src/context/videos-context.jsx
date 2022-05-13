3bb46e47966237bf000599624f911e6b69e754cc
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-13 11:19:06 +0530

    feat: call getVideos function in context

--- src/context/videos-context.jsx
+++ src/context/videos-context.jsx
@@ -1,9 +1,14 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useState, useEffect } from "react";
+import { getVideos } from "../utils";
 
 const VideoContext = createContext(null);
 
 const VideoProvider = ({ children }) => {
   const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    getVideos(setVideos);
+  },[]);
 
   return (
     <VideoContext.Provider value={videos}>{children}</VideoContext.Provider>
