0abf3a7784ed8e42f3fdfe75b2ca7f4a29959096
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-14 12:13:29 +0530

    feat: add onChange handler for login page

--- src/pages/Authentication/Login/Login.jsx
+++ src/pages/Authentication/Login/Login.jsx
@@ -1,20 +1,40 @@
 import "./../Auth.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Login = () => {
+  const [user, setUser] = useState({ email: "", password: "" });
+  const loginHandler = () => {};
+  const guestLoginHandler = () => {};
   return (
     <main className="main-wrapper">
       <form className="auth-container">
         <h1 className="auth-title">Login</h1>
         <p className="auth-subtitle">Please enter your email and password</p>
         <div className="input-container standard auth-email">
-          <input type="email" id="email" placeholder=" " value="" />
+          <input
+            type="email"
+            id="email"
+            placeholder=" "
+            value={user.email}
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+            }}
+          />
           <label htmlFor="email">Enter Your Email</label>
           <small>Enter Valid Email</small>
         </div>
 
         <div className="input-container standard auth-password">
-          <input type="password" id="password" placeholder=" " value="" />
+          <input
+            type="password"
+            id="password"
+            placeholder=" "
+            value={user.password}
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+            }}
+          />
           <label htmlFor="password">Enter Password</label>
           <small>Enter Valid Password</small>
         </div>
@@ -32,10 +52,18 @@ export const Login = () => {
           </p>
         </div>
 
-        <button type="submit" className="button btn-info btn-login">
+        <button
+          type="submit"
+          className="button btn-info btn-login"
+          onClick={loginHandler}
+        >
           Login
         </button>
-        <button type="submit" className="button btn-info btn-login">
+        <button
+          type="submit"
+          className="button btn-info btn-login"
+          onClick={guestLoginHandler}
+        >
           Login with Guest Credentials
         </button>
         <hr className="divider" />
