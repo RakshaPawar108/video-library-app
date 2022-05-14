cc9a74dbba12e5744284b7597bbd51d5fa9cb28b
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-14 12:20:40 +0530

    feat: add onChange handler to signup page

--- src/pages/Authentication/Signup/Signup.jsx
+++ src/pages/Authentication/Signup/Signup.jsx
@@ -1,28 +1,71 @@
 import "./../Auth.css";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export const Signup = () => {
+  const [user, setUser] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const signupHandler = () => {};
+
   return (
     <main className="main-wrapper">
       <form className="auth-container" action="" method="post">
         <h1 className="auth-title">Signup</h1>
         <p className="auth-subtitle">Create an Account</p>
         <div className="input-container standard auth-name">
-          <input type="text" id="firstName" placeholder=" " value="" />
+          <input
+            type="text"
+            id="firstName"
+            placeholder=" "
+            value={user.firstName}
+            onChange={(e) => {
+              setUser({ ...user, firstName: e.target.value });
+            }}
+          />
           <label htmlFor="firstName">Enter Your First Name</label>
         </div>
         <div className="input-container standard auth-name">
-          <input type="text" id="lastName" placeholder=" " value="" />
+          <input
+            type="text"
+            id="lastName"
+            placeholder=" "
+            value={user.lastName}
+            onChange={(e) => {
+              setUser({ ...user, lastName: e.target.value });
+            }}
+          />
           <label htmlFor="lastName">Enter Your Last Name</label>
         </div>
         <div className="input-container standard auth-email">
-          <input type="email" id="email" placeholder=" " value="" />
+          <input
+            type="email"
+            id="email"
+            placeholder=" "
+            value={user.email}
+            onChange={(e) => {
+              setUser({ ...user, email: e.target.value });
+            }}
+          />
           <label htmlFor="email">Enter Your Email</label>
           <small>Enter Valid Email</small>
         </div>
 
         <div className="input-container standard auth-password">
-          <input type="password" id="password" placeholder=" " value="" />
+          <input
+            type="password"
+            id="password"
+            placeholder=" "
+            value={user.password}
+            onChange={(e) => {
+              setUser({ ...user, password: e.target.value });
+            }}
+          />
           <label htmlFor="password">Enter Password</label>
           <small>Enter Valid Password</small>
         </div>
@@ -32,13 +75,20 @@ export const Signup = () => {
             type="password"
             id="confirmPassword"
             placeholder=" "
-            value=""
+            value={user.confirmPassword}
+            onChange={(e) => {
+              setUser({ ...user, confirmPassword: e.target.value });
+            }}
           />
           <label htmlFor="confirmPassword">Confirm Password</label>
           <small>Enter Valid Password</small>
         </div>
 
-        <button type="submit" className="button btn-info btn-signup">
+        <button
+          type="submit"
+          className="button btn-info btn-signup"
+          onClick={signupHandler}
+        >
           Sign Up
         </button>
         <hr className="divider" />
