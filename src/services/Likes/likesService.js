9fd9c25c1f90465a249b33021e3c200575d38238
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-16 08:32:40 +0530

    feat: add service call for liked videos

---
+++ src/services/Likes/likesService.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export const likesService = (token, video) => {
+  axios.post(
+    "/api/user/likes",
+    { video },
+    { headers: { authorization: token } }
+  );
+};
--- src/services/index.js
+++ src/services/index.js
@@ -1,3 +1,4 @@
 export { fetchVideos } from "./Videos/fetchVideos";
 export { logIn } from "./Authentication/login-service";
 export { signUp } from "./Authentication/signup-service";
+export {likesService} from './Likes/likesService'
