91e5d18fc868f2dd6e273d57c3b0adbb08055b99
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-16 18:37:36 +0530

    feat: add private routing so user can visit all pages only if logged in

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -1,7 +1,12 @@
 import { NavLink } from "react-router-dom";
+import { useAuth } from "../../context";
 import "./SideNav.css";
 
 const SideNav = () => {
+  const {
+    authState: { token },
+  } = useAuth();
+
   return (
     <aside className="sidenav-aside">
       <nav className="sidenav-navbar">
@@ -13,25 +18,25 @@ const SideNav = () => {
             </NavLink>
           </li>
           <li className="sidenav-item">
-            <NavLink to="/liked">
+            <NavLink to={token ? "/liked" : "/login"}>
               <i className="fas fa-thumbs-up sidenav-icon"></i>
               Liked
             </NavLink>
           </li>
           <li className="sidenav-item">
-            <NavLink to="/playlists">
+            <NavLink to={token ? "/playlists" : "/login"}>
               <i className="fas fa-list sidenav-icon"></i>
               Playlists
             </NavLink>
           </li>
           <li className="sidenav-item">
-            <NavLink to="/watch-later">
+            <NavLink to={token ? "/watch-later" : "/login"}>
               <i className="fas fa-clock sidenav-icon"></i>
               Watch Later
             </NavLink>
           </li>
           <li className="sidenav-item">
-            <NavLink to="/history">
+            <NavLink to={token ? "/history" : "/login"}>
               <i className="fas fa-history sidenav-icon"></i>
               History
             </NavLink>
