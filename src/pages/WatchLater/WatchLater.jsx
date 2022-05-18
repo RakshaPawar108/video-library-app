03f9d085b032b5ac70d2d53b6ae9e7d16bebe290
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-18 11:01:19 +0530

    feat: display videos on watch later page and remove video from that page functionality

--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -1,22 +1,48 @@
 import "./WatchLater.css";
 import { HorizontalVideoCard, PageSidePiece, SideNav } from "../../components";
+import { useAuth, useWatchLater } from "../../context";
+import { removeFromWatchLater } from "../../utils";
+import { useState } from "react";
 
 export const WatchLater = () => {
+  const {
+    authState: { token },
+  } = useAuth();
+  const {
+    watchLaterState: { watchlater },
+    watchLaterDispatch,
+  } = useWatchLater();
+
+  const [watchLater, setWatchLater] = useState(true);
+
+  const removeVidFromWatchLater = (_id) => {
+    removeFromWatchLater(_id, token, watchLaterDispatch, setWatchLater);
+  };
   return (
     <div className="watch-later-wrapper">
       <SideNav />
       <div className="videos-container">
-        <PageSidePiece pageTitle="Watch Later" numVideos="0" />
+        <PageSidePiece pageTitle="Watch Later" numVideos={watchlater.length} />
         <ul className="list list-stacked videos-list">
-          <li className="list-item stacked">
-            <HorizontalVideoCard
-              videoId="ID"
-              videoThumbnail="https://picsum.photos/300"
-              videoTime="1:00"
-              videoTitle="Video Title"
-              channelName="Channel Name"
-            />
-          </li>
+          {watchlater.length > 0 ? (
+            watchlater.map((watchLaterVideo) => (
+              <li className="list-item stacked" key={watchLaterVideo._id}>
+                <HorizontalVideoCard
+                  videoId={watchLaterVideo._id}
+                  videoThumbnail={watchLaterVideo.videoThumbnail}
+                  videoTime={watchLaterVideo.videoTime}
+                  videoTitle={watchLaterVideo.title}
+                  channelName={watchLaterVideo.channelName}
+                  removeVideo={removeVidFromWatchLater}
+                  watchlater={watchLater}
+                />
+              </li>
+            ))
+          ) : (
+            <div className="no-video-div">
+              You have not added any videos to Watch Later yet
+            </div>
+          )}
         </ul>
       </div>
     </div>
