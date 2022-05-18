141bb4edd3e1272b757e788bf2742623da6f63c5
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-18 08:54:15 +0530

    feat: add to watchLater service call

--- src/services/index.js
+++ src/services/index.js
@@ -4,3 +4,4 @@ export { signUp } from "./Authentication/signup-service";
 export { likesService } from "./Likes/likesService";
 export { removeLikesService } from "./Likes/removeLikesService";
 export { fetchLikesService } from "./Likes/fetchLikesService";
+export { addWatchLaterService } from "./WatchLater/addWatchLaterService";
