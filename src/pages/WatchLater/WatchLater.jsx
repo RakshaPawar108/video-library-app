495f2fce9bf9df31ee7a39c0b3c474fc159f152b
Raksha Pawar <rakshapawar10.sae@gmail.com>
2022-05-18 08:27:24 +0530

    feat: add sidepiece component and horizontal video card component to watchlater page

--- src/pages/WatchLater/WatchLater.jsx
+++ src/pages/WatchLater/WatchLater.jsx
@@ -1,11 +1,24 @@
 import "./WatchLater.css";
-import { SideNav } from "../../components";
+import { HorizontalVideoCard, PageSidePiece, SideNav } from "../../components";
 
 export const WatchLater = () => {
   return (
     <div className="watch-later-wrapper">
       <SideNav />
-      <div className="videos-container">This is Watch Later Page</div>
+      <div className="videos-container">
+        <PageSidePiece pageTitle="Watch Later" numVideos="0" />
+        <ul className="list list-stacked videos-list">
+          <li className="list-item stacked">
+            <HorizontalVideoCard
+              videoId="ID"
+              videoThumbnail="https://picsum.photos/300"
+              videoTime="1:00"
+              videoTitle="Video Title"
+              channelName="Channel Name"
+            />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
